a0973bbb3fb741b2caedf248b9b89752b1c2f001
Mweya Ruider <mweyaruider@gmail.com>
2022-07-30 17:42:35 +0200

    temporary password check

--- main.py
+++ main.py
@@ -4,12 +4,7 @@ from deta import Deta
 
 app = FastAPI()
 deta = Deta()
-
-dbgSplashpage = f"""
-<header>
-    <title>WSS Debug Page</title>
-</header>
-<body><pre style="color:purple;">
+logo = """
  ▄         ▄  ▄▄▄▄▄▄▄▄▄▄▄  ▄▄▄▄▄▄▄▄▄▄▄ 
 ▐░▌       ▐░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
 ▐░▌       ▐░▌▐░█▀▀▀▀▀▀▀▀▀ ▐░█▀▀▀▀▀▀▀▀▀ 
@@ -22,24 +17,59 @@ dbgSplashpage = f"""
 ▐░░▌     ▐░░▌▐░░░░░░░░░░░▌▐░░░░░░░░░░░▌
  ▀▀       ▀▀  ▀▀▀▀▀▀▀▀▀▀▀  ▀▀▀▀▀▀▀▀▀▀▀ 
                                        
-Powered by <a href="https://deta.sh">deta.sh</a> 
+Powered by <a href="https://deta.sh">deta.sh</a>"""
 
+debug_page = f"""
+<header>
+    <title>WSS Debug Page</title>
+</header>
+<body><pre style="color:purple;">
+{logo}
+</pre>
 
-Deta Project:\t{deta.project_id}
-Client Type:\t{os.getenv("client_type")}
-Instance Slug:\t{os.getenv("DETA_PATH")}
-Running on Micro:\t{os.getenv("DETA_RUNTIME")}
+Deta Project:\t\t\t{deta.project_id}
+Client Type:\t\t\t{os.getenv("client_type")}
+Instance Slug:\t\t\t{os.getenv("DETA_PATH")}
+Running on Micro:\t\t\t{os.getenv("DETA_RUNTIME")}
 
-</pre></body>
+</body>
+"""
+
+splash_screen = f"""
+<header>
+    <title>WSS Debug Page</title>
+</header>
+<body><pre style="color:purple;">
+{logo}
+</pre>
+<form action="" method="post">
+    <button name="foo" value="upvote">Upvote</button>
+</form>
+</body>
 """
 
 @app.get("/")
-async def init():
-    # Password check
+async def splash():
+    # Password store
+    base = deta.Base("wss")
+    try:
+        base.insert("password", os.getenv("password")) # type: ignore   The env will always be set and even if it's not it will fail gracefully
+    except:
+        print("[!] Password already set, cannot reset")
+
     # Loading animation
     # Redirect to done
-    return Response(dbgSplashpage, 200, {}, "text/html")
+    return Response(splash_screen, media_type="text/html")
 
+
+@app.post("/init")
+async def init(password: str):
+    base = deta.Base("wss")
+    if base.get("password") == password:
+        return Response(debug_page, media_type="text/html")
+    else:
+        return Response("Unauthorized", 403)
+    # Check if ran first in DB
 
 @app.get("/config")
 async def get_config():
@@ -50,5 +80,5 @@ async def get_config():
 
 
 @app.get("/console")
-async def showConsole():
+async def get_console():
     return "TODO"
